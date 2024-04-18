56e8c3cbcc279362f70a5c68c023a03291f0faa6
Garreau Valentin <garreauvalentin@air-de-garreau.home>
2024-04-18 16:13:46 +0200

    graphsV2

--- visualisation_donnees.R
+++ visualisation_donnees.R
@@ -38,23 +38,71 @@ for (i in 1:length(fichier_list)) {
 }
 
 
-# Création des graphs 
+# Création des graphs sur jeu de données trnl avec colonne espèces genetiques
 ###Nombre de crottes/espèces###
-table(table12s_clean$espece)
-barplot(table(table12s_clean$espece), xlab = "Espèces", ylab =  "Nombre d'échantillons", main = "Nombres d'échantillons pour chaque Espèces")  
+barplot(table(tabletnrl_clean$espece_gen), xlab = "Espèces", ylab =  "Nombre d'échantillons", main = "Nombres d'échantillons pour chaque Espèces")  
 
 ###Nombre de crottes/espèces/sites###
-ggplot(as.data.frame(table(table12s_clean$espece, table12s_clean$massif)), aes(x = Var2, y = Freq, fill = Var1) + 
-         geom_bar(stat = "identity") +
-         labs(x = "Massif", y = "Nombre d'espèces échantillonnées", title = "Nombre d'espèces échantillonnées par massif") +
-         theme_minimal()+
-         scale_fill_discrete(name = "Espèces"))
+ggplot(as.data.frame(table(tabletnrl_clean$espece_gen, tabletnrl_clean$massif)), aes(x = Var2, y = Freq, fill = Var1)) + 
+  geom_bar(stat = "identity") +
+  labs(x = "Massif", y = "Nombre d'espèces échantillonnées", title = "Nombre d'espèces échantillonnées par massif") +
+  theme_minimal() +
+  scale_fill_discrete(name = "Espèces")
 
 ###Nombre d'espèces/moyens de conservations###
-ggplot(as.data.frame(table(table12s_clean$espece, table12s_clean$conservation)),aes(x = Var2, y = Freq, fill = Var1)) +
+ggplot(as.data.frame(table(tabletnrl_clean$espece_gen, tabletnrl_clean$conservation)),aes(x = Var2, y = Freq, fill = Var1)) +
   geom_bar(stat = "identity") +
   labs(x = "Moyens de conservation", y = "Nombre d'espèces échantillonnées", title = "Nombre d'espèces échantillonnées par moyens de conservation") +
   theme_minimal()+
   scale_fill_discrete(name = "Espèces")
 
 ###Nombre de crottes/mois###
+tabletnrl_clean$date <- gsub("-", "/", tabletnrl_clean$date)
+tabletnrl_clean$month<-as.numeric(strftime(tabletnrl_clean$date,format="%m"))
+barplot(table(tabletnrl_clean$month), xlab = "Mois", ylab = "Nombre d'échantillons", main = "Nombre d'échantillons par mois")
+
+###Nombre de crottes/mois/sites###
+ggplot(as.data.frame(table(tabletnrl_clean$month, tabletnrl_clean$massif)),aes(x = Var2, y = Freq, fill = Var1)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Moyens de conservation", y = "Nombre d'espèces échantillonnées", title = "Nombre d'espèces échantillonnées par mois") +
+  theme_minimal()+
+  scale_fill_discrete(name = "Mois")
+
+###Nombre d'echantillons/Fraicheur###
+sum(is.na(tabletnrl_clean$fraicheur))
+barplot(table(tabletnrl_clean$fraicheur, useNA = "ifany"), xlab = "État de fraicheur", ylab = "Nombres d'échantillons", main = "Nombres d'échantillons par état de fraicheur")
+
+###Liste expèces/altitude###
+tabletnrl_clean$Tranche_altitude <- cut(as.numeric(tabletnrl_clean$altitude), breaks = seq(1500, 2500, by = 200), labels = FALSE)
+for (i in 1:length(seq(1500, 2500, by = 200))) {
+  cat("Tranche", i, ":", seq(1500, 2500, by = 200)[i], "-", seq(1500, 2500, by = 200)[i+1], "m\n")
+}
+ggplot(as.data.frame(table(tabletnrl_clean$espece_gen, tabletnrl_clean$Tranche_altitude)), aes(x = Var2, y = Freq, color = Var1)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Altitude (m)", y = "Nombre d'espèces", title = "Nombre d'espèces par altitude") +
+  theme_minimal()
+
+###Nombre d'echantillons/mois/moyens de conservation/espèces###
+ggplot(as.data.frame (table(tabletnrl_clean$month, tabletnrl_clean$espece_gen ,tabletnrl_clean$conservation)), aes(x = Var1, Var2, y = Freq, fill = Var2)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Moyen de conservation", y = "Nombre d'échantillons", title = "Nombres d'échantillons par espèces et par moyen de conservation secteur Chamonix") +
+  theme_minimal() +
+  theme(axis.text.x = element_text (hjust = 1)) +
+  scale_fill_discrete(name = "Espèces")
+
+###Espèces supposées vs Espèces séquencées###
+as.data.frame(table(tabletnrl_clean$espece_sup, tabletnrl_clean$espece_gen))
+ggplot(as.data.frame(table(tabletnrl_clean$espece_sup, tabletnrl_clean$espece_gen)), aes(x = Var1, y = Var2, fill = Freq)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(x = "Espèces supposées", y = "Espèces séquencées", title = "Comparaison des espèces") +
+  theme_minimal()
+ggplot(as.data.frame(table(tabletnrl_clean$espece_sup, tabletnrl_clean$espece_gen)), aes(x = Var1, y = Freq, fill = Var2)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Espèces supposées", y = "Occurrences", title = "Comparaison des occurrences par espèce supposée") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  scale_fill_discrete(name = "Espèces séquencées") 
+
+
