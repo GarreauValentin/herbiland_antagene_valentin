cf7bad4780175de7888354824f3044e2a5a0d8a6
Garreau Valentin <garreauvalentin@air-de-garreau.home>
2024-04-25 16:18:14 +0200

    formattages v3

--- formattage_fichiers.R
+++ formattage_fichiers.R
@@ -5,16 +5,16 @@ library(ggplot2)
 table12s <- read_xlsx("data/01_F0764_CREA_MontBlanc_Ongulés_resultats_12S/F0764-CREA_Regime_Ongules-12S-plq01et02_Metadata_seuil_100-100-100.xlsx")
 tabletrnl <- read_xlsx("data/02_F0764_CREA_MontBlanc_Ongulés_resultats_trnL/F0764-CREA_Regime_Ongules-trnL-plq01et02_Metadata_seuil_100-100-100.xlsx")
 tablecype <- read_xlsx("data/04_F0764_CREA_MontBlanc_Ongulés_resultats_Cype/F0764-CREA_Regime_Ongules-Cype-plq01et02_Metadata_seuil_100-100-100.xlsx")
-tableaste <- read_xlsx("data/03_F0764_CREA_MontBlanc_Ongulés_resultats_Aste/F0764-CREA_Regime_Ongules-Aste-plq01et02_Metadata_seuil_bruts.xlsx")
+tableaste <- read_xlsx("data/03_F0764_CREA_MontBlanc_Ongulés_resultats_Aste/F0764-CREA_Regime_Ongules-Aste-plq01et02_Metadata_seuil_100-100-100.xlsx")
 tablepoac <- read_xlsx("data/05_F0764_CREA_MontBlanc_Ongulés_resultats_Poac/F0764-CREA_Regime_Ongules-Poac-plq01et02_Metadata_seuil_100-100-100.xlsx")
-
+doublons <- read_xlsx("data/Doublons_crottes_total.xlsx", col_names = TRUE)
 
 ### Formattage table 12S - Identification espece 
 # Creation nouveaux noms de colonne
 table12s <- table12s[-c(1,2),]
 colnames(table12s) <- c("espece", "prelevement", "id_interne", "date", "observateur",
                         "commune", "site", "longitude", "latitude", "sexe", "conservation", "altitude",
-                        "fraicheur", "milieu", "remarques", "id_antagene", "qualite",
+                        "fraicheur", "milieu", "remarques", "N_Antagene", "qualite",
                         "abondance_micro", "abondance_taxons_nc", "abondance_cibles",
                         "occurrence_micro", "occurrence_taxons_nc", "occurrence_cibles",
                         "genus_bos", "species_ibex", "species_ovis", "species_rupircapra", "species_capreolus", "species_elaphus")
@@ -34,8 +34,10 @@ table12s[table12s$conservation == "Alcool 90 denaturee",]$conservation <- "denat
 table12s$fraicheur[table12s$fraicheur == "frais" & !is.na(table12s$fraicheur)] <- "oui"
 table12s[table12s$massif == "belledonne",]$altitude <- "2030"
 
-
 write.csv(file = "output/12s_table.csv", table12s)
+#création de fichier doublons
+table12s <- table12s[table12s$N_Antagene %in% doublons$N_Antagene, ]
+write.csv(file = paste0("output/12s_doublons.csv"), table12s)
 
 ### Formattage tables tnrl, aste, cype et poac 
 # Remplacements noms de colonnes communes a tous les fichiers
@@ -62,7 +64,7 @@ for (i in 1:length(fichier_list)) {
   
   colnames(df)[c(1:24)] <- c("espece_sup", "espece_gen", "prelevement", "id_interne", "date", "observateur",
                              "commune", "site", "longitude", "latitude", "sexe", "conservation", "altitude",
-                             "fraicheur", "milieu", "remarques", "id_antagene", "qualite",
+                             "fraicheur", "milieu", "remarques", "N_Antagene", "qualite",
                              "abondance_micro", "abondance_taxons_nc", "abondance_cibles",
                              "occurrence_micro", "occurrence_taxons_nc", "occurrence_cibles")
   
@@ -94,32 +96,46 @@ for (i in 1:length(fichier_list)) {
 assignation_nom_niveau <- rbind(tnrl_niveau, poac_niveau, aste_niveau, cype_niveau)  
 assignation_nom_niveau <- assignation_nom_niveau %>% dplyr::distinct(niveau, nom)
 write.csv(file = "output/assignation_nom_niveau.csv", assignation_nom_niveau)
+#création doublons
+for (i in 1:length(fichier_list)) {
+  df <- df[df$N_Antagene %in% doublons$N_Antagene, ]
+  df$doublons <- doublons[doublons$N_Antagene %in% df$N_Antagene, ]$doublons
+  assign(paste0(base_nom[i]), df)
+  write.csv(file = paste0("output/", base_nom[i], "_doublons.csv"), df)
+}
 
-###Feuille 3###
+###Formattage feuille 3###
 #chargement des données taxonomie occurence
 taxotrnl <- read_xlsx("data/02_F0764_CREA_MontBlanc_Ongulés_resultats_trnL/F0764-CREA_Regime_Ongules-trnL-plq01et02_Metadata_seuil_100-100-100.xlsx", sheet = "Taxonomie_Occurrences seuil")
 taxocype <- read_xlsx("data/04_F0764_CREA_MontBlanc_Ongulés_resultats_Cype/F0764-CREA_Regime_Ongules-Cype-plq01et02_Metadata_seuil_100-100-100.xlsx", sheet = "Taxonomie_Occurrences seuil")
-taxoaste <- read_xlsx("data/03_F0764_CREA_MontBlanc_Ongulés_resultats_Aste/F0764-CREA_Regime_Ongules-Aste-plq01et02_Metadata_seuil_bruts.xlsx", sheet = "Taxonomie_Occurrences")
+taxoaste <- read_xlsx("data/03_F0764_CREA_MontBlanc_Ongulés_resultats_Aste/F0764-CREA_Regime_Ongules-Aste-plq01et02_Metadata_seuil_100-100-100.xlsx", sheet = "Taxonomie_Occurrences seuil")
 taxopoac <- read_xlsx("data/05_F0764_CREA_MontBlanc_Ongulés_resultats_Poac/F0764-CREA_Regime_Ongules-Poac-plq01et02_Metadata_seuil_100-100-100.xlsx", sheet = "Taxonomie_Occurrences seuil")
+taxo12s <- read_xlsx("data/01_F0764_CREA_MontBlanc_Ongulés_resultats_12S/F0764-CREA_Regime_Ongules-12S-plq01et02_Metadata_seuil_100-100-100.xlsx", sheet = "Taxonomie_Occurrences seuil")
 
-### Formattage tables tnrl, aste, cype et poac 
-# Remplacements noms de colonnes communes a tous les fichiers
-fichier_list <- list(taxotrnl,
+### Formattage tables 12s, tnrl, aste, cype et poac 
+fichier_list2 <- list(taxotrnl,
                      taxocype,
                      taxopoac,
-                     taxoaste)
-
+                     taxoaste,
+                     taxo12S)
 base_nom <- c("tnrl",
               "cype",
               "poac",
-              "aste")
+              "aste",
+              "12s")
 
-for (i in 1:length(fichier_list)) {
-  df3 <- fichier_list[[i]]
+for (i in 1:length(fichier_list2)) {
+  df2 <- fichier_list2[[i]]
   
-  colnames(df3)[c(1:14)] <- c("base", "poucentage_identité", "domaine", "sous_regne", "infra-regne", "classe", "famille", 
-                             "genre", "espece", "rang", "nom_scientifique", "liste_espece", "total" )
-  colnames(df3)[c(15:dim(df)[2])] <- df[2,c(15:dim(df)[2])]
-  df3 <- df3[-c(1:2),]
+  colnames(df2)[c(1:21)] <- c("base", "meilleure_identité", "domaine", "sous_regne", 
+                              "infra-regne", "classe", "ordre", "famille", "genre", "espece", 
+                              "rang", "nom_scientifique", "liste_espece", "occurence_total", 
+                              "Non analysable", "bouquetin", "cerf", "chamois", 
+                              "chevreuil", "mouton", "vache" )
+  df2 <- df2[-c(1:2),]
+
+  assign(paste0(base_nom[i], "_taxon_table"), df2)
+  
+  write.csv(file = paste0("output/", base_nom[i], "_table_taxon.csv"), df2)
 }
 
